5e27ebe554f0692401377fe1ad388cda26ad1262
clever411 <clever411@yandex.ru>
2018-06-22 18:30:16 +0300

    init commit
    Первый коммит. Сдесь сделан класс ChartPrinter, который
    представляет из себя инструмент для рисования графиков.
    Сделано автоматическое определение размера, чтобы все
    графики поместились полностью. Единственная проблема:
    минимум не может быть больше нуля, также как и максимум
    не может быть меньше нуля. Программа хорошо и стабильно
    работает.
    
    Что нужно сделать:
    	- сделать класс Transformable;
    	- создать разметку;
    	- создать исправить проблему с нулями(см. выше);
    	- создать возможность ручного выбора размера;
    	- разобраться со сглаживанием.

---
+++ chart_printer/ChartPrinter.hpp
@@ -0,0 +1,371 @@
+#ifndef CHART_PRINTER_HPP
+#define CHART_PRINTER_HPP
+
+#include <algorithm>
+#include <cmath>
+#include <list>
+#include <memory>
+#include <vector>
+
+#include <clever/SFML/HelpFunctions.hpp>
+
+#include <SFML/Graphics.hpp>
+
+
+
+
+
+template<typename T = void>
+class Line: public sf::Drawable
+{
+public:
+	Line(
+		sf::Vector2f const &begin = {0.0f, 0.0f},
+		sf::Vector2f const &end = {0.0f, 0.0f},
+		sf::Color const &color = sf::Color::Black,
+		float thickness = 1.0f
+	)
+	{
+		setPosition(begin, end);
+		setColor(color);
+		setThickness(thickness);
+		return;
+	}
+
+	void draw(
+		sf::RenderTarget &target,
+		sf::RenderStates states = sf::RenderStates::Default
+	) const override
+	{
+		target.draw(rect_, states);
+		return;
+	}
+	
+	Line &setThickness(float thickness)
+	{
+		rect_.setSize({rect_.getSize().x, thickness});
+		return *this;
+	}
+	float getThickness() const
+	{
+		return rect_.getSize().y;
+	}
+
+	Line &setPosition(
+		sf::Vector2f const &begin, 
+		sf::Vector2f const &end
+	) 
+	{
+		rect_.setPosition(begin);
+		rect_.setSize({
+			std::hypot(end.x-begin.x, end.y-begin.y),
+			rect_.getSize().y
+		});
+		rect_.setOrigin(0.0f, rect_.getSize().y/2);
+		rect_.setRotation(
+			clever::togradus(clever::angle(
+				end.x-begin.x, end.y-begin.y, 1.0f, 0.0f
+			))
+		);
+		return *this;
+	}
+
+	Line &setColor(sf::Color const &newcolor)
+	{
+		rect_.setFillColor(newcolor);
+		return *this;
+	}
+	sf::Color const &getColor() const
+	{
+		return rect_.getFillColor();
+	}
+private:
+	sf::RectangleShape rect_;
+
+};
+
+
+
+
+
+struct ChartSettings
+{
+	sf::Color color;
+	float thickness;
+	float overlayprior;
+
+	static ChartSettings const Default;
+};
+ChartSettings const ChartSettings::Default =
+	ChartSettings{sf::Color::Black, 2.0f, 0.0f};
+
+
+
+
+struct AxisSettings
+{
+	sf::Color color;
+	float thickness;
+
+	static AxisSettings const Default;
+};
+AxisSettings const AxisSettings::Default =
+	AxisSettings{sf::Color::Black, 1};
+
+
+
+
+template<typename T = float>
+class ChartPrinter: public sf::Drawable
+{
+public:
+	typedef T value_type;
+	typedef std::shared_ptr<
+		std::vector<std::pair<value_type, value_type>>
+	> chart_type;
+
+
+
+
+	void update()
+	{
+		if(ischanged_)
+			adjust_();
+		return;
+	}
+
+	void draw(
+		sf::RenderTarget &target,
+		sf::RenderStates states = sf::RenderStates::Default
+	) const override
+	{
+		target.draw(sprite_, states);
+		return;
+	}
+
+
+	ChartPrinter &addChart(
+		chart_type newchart,
+		ChartSettings const &settings = ChartSettings::Default
+	)
+	{
+		charts_.push_back({newchart, settings});
+		ischanged_ = true;
+		return *this;
+	}
+	ChartPrinter &removeChart(chart_type const &chart)
+	{
+		for(auto b = charts_.begin(), e = charts_.end(); b != e; ++b) {
+			if(b->first == chart) {
+				charts_.erase(b);
+				ischanged_ = true;
+				break;
+			}
+		}
+		return *this;
+	}
+	ChartPrinter &clearCharts()
+	{
+		charts_.clear();
+		return *this;
+	}
+
+
+	AxisSettings const &getAxisSettings() const
+	{
+		return axis_;
+	}
+	ChartPrinter &setAxisSettings(AxisSettings const &newaxis)
+	{
+		axis_ = newaxis;
+		ischanged_ = true;
+		return *this;
+	}
+
+
+	ChartPrinter &setSize(float width, float height)
+	{
+		width_ = width;
+		height_ = height;
+		return *this;
+	}
+	ChartPrinter &setSize(sf::Vector2f size)
+	{
+		width_ = size.x;
+		height_ = size.y;
+		return *this;
+	}
+
+	sf::Vector2f getSize() const
+	{
+		return {width_, height_};
+	}
+
+private:
+
+	void adjust_()
+	{
+		// for overlay
+		std::sort(
+			charts_.begin(), charts_.end(),
+			[](
+				std::pair<chart_type, ChartSettings> const &lhs,
+				std::pair<chart_type, ChartSettings> const &rhs
+			)->bool {
+				return lhs.second.overlayprior < rhs.second.overlayprior;
+			}
+		);
+		
+		// find point for axis
+		value_type xmin = 0, xmax = 0;
+		value_type ymin = 0, ymax = 0;
+
+		typename std::vector<
+			std::pair<value_type, value_type>
+		>::const_iterator
+			buf
+		;
+
+		for(auto b = charts_.begin(), e = charts_.end(); b != e; ++b) {
+			buf = std::min_element(
+				b->first->cbegin(),
+				b->first->cend(),
+				[](
+					std::pair<value_type, value_type> const &lhs,
+					std::pair<value_type, value_type> const &rhs
+				)->bool {
+					return lhs.first < rhs.first;
+				}
+			);
+
+			if(buf != b->first->cend() && buf->first < xmin)
+				xmin = buf->first;
+
+			buf = std::max_element(
+				b->first->cbegin(),
+				b->first->cend(),
+				[](
+					std::pair<value_type, value_type> const &lhs,
+					std::pair<value_type, value_type> const &rhs
+				)->bool {
+					return lhs.first < rhs.first;
+				}
+			);
+
+			if(buf != b->first->cend() && buf->first > xmax)
+				xmax = buf->first;
+
+			buf = std::min_element(
+				b->first->cbegin(),
+				b->first->cend(),
+				[](
+					std::pair<value_type, value_type> const &lhs,
+					std::pair<value_type, value_type> const &rhs
+				)->bool {
+					return lhs.second < rhs.second;
+				}
+			);
+			if(buf != b->first->cend() && buf->second < ymin)
+				ymin = buf->second;
+
+			buf = std::max_element(
+				b->first->cbegin(),
+				b->first->cend(),
+				[](
+					std::pair<value_type, value_type> const &lhs,
+					std::pair<value_type, value_type> const &rhs
+				)->bool {
+					return lhs.second < rhs.second;
+				}
+			);
+			if(buf != b->first->cend() && buf->second > ymax)
+				ymax = buf->second;
+		}
+		
+		float xl = xmax-xmin;
+		float yl = ymax-ymin;
+
+		if(xl == 0)
+			axispoint_.x = 0.0f;
+		else
+			axispoint_.x = float(-xmin) / xl * width_;
+		if(yl == 0)
+			axispoint_.y = 0.0f;
+		else
+			axispoint_.y = float(-ymin) / yl * height_;
+
+
+		// draw
+		rtexture_.create(width_, height_);
+		rtexture_.clear(sf::Color::Transparent);
+
+		Line<> line;
+		line.setThickness(axis_.thickness);
+		line.setColor(axis_.color);
+
+		line.setPosition(
+			{0.0f, axispoint_.y+1},
+			{width_, axispoint_.y+1}
+		);
+		rtexture_.draw(line);
+
+		line.setPosition(
+			{axispoint_.x+1, 0.0f},
+			{axispoint_.x+1, height_}
+		);
+		rtexture_.draw(line);
+
+		for(auto const &i : charts_) {
+			if(i.first->size() < 2)
+				continue;
+
+			line.setColor(i.second.color);
+			line.setThickness(i.second.thickness);
+
+			for(auto b = i.first->begin()+1, e = i.first->end(); b != e; ++b) {
+				line.setPosition(
+					{
+						(b-1)->first/xl*width_ + axispoint_.x,
+						((b-1)->second/yl*height_ + axispoint_.y)
+					},
+					{
+						b->first/xl*width_ + axispoint_.x,
+						(b->second/yl*height_ + axispoint_.y)
+					}
+				);
+				rtexture_.draw(line);
+			}
+
+		}
+
+		sprite_.setTexture(rtexture_.getTexture());
+		sprite_.setTextureRect(
+			sf::IntRect(0, 0, int(width_), int(height_))
+		);
+
+		ischanged_ = false;
+		return;
+	}
+
+	std::vector<
+		std::pair<chart_type, ChartSettings>
+	> charts_;
+
+	AxisSettings axis_ = AxisSettings::Default;
+	sf::Vector2f axispoint_ = {0.0f, 0.0f};
+
+	float width_ = 300.0f, height_ = 300.0f;
+
+
+	bool ischanged_ = true;
+
+	sf::RenderTexture rtexture_;
+	sf::Sprite sprite_;
+
+};
+
+
+
+
+
+#endif
---
+++ chart_printer/main.cpp
@@ -0,0 +1,99 @@
+#include <cmath>
+#include <iostream>
+#include <functional>
+
+#include <clever/SFML/HelpFunctions.hpp>
+
+using namespace clever;
+#include "ChartPrinter.hpp"
+
+using namespace sf;
+using namespace std;
+
+
+RenderWindow window;
+VideoMode dsmode;
+string const TITLE = "Chart Printer";
+Event event;
+
+void init_window()
+{
+	ContextSettings sets;
+	sets.antialiasingLevel = 8;
+	dsmode = VideoMode::getDesktopMode();
+	window.create(dsmode, TITLE, Style::None, sets);
+	window.setPosition({0, 0});
+	window.setVerticalSyncEnabled(true);
+	return;
+}
+
+
+typedef shared_ptr<
+	vector<pair<float, float>>
+> chart_type;
+
+chart_type generate_chart(
+	float start, float finish, float step,
+	function<float(float)> const &fun
+)
+{
+	chart_type chart(new typename chart_type::element_type());
+	for(; start < finish; start += step) {
+		chart->push_back({start, fun(start)});
+	}
+
+	return chart;
+}
+
+int main( int argc, char *argv[] )
+{
+	init_window();
+	
+	ChartPrinter<float> chart;
+	chart.setSize(
+		conversion<float>(window.getSize())
+	);
+	chart.setAxisSettings( AxisSettings{Color::Black, 10.0f} );
+	chart.addChart(
+		generate_chart(
+			0.0f, 1920.0f, 1.0f, 
+			[](float x)->float {
+				return sqrt(x);
+			}
+		),
+		{ Color::Black, 2.0f, 10.0f }
+	);
+	chart.addChart(
+		generate_chart(
+			0.0f, 1920.0f, 4.0f, 
+			[](float x)->float {
+				return pow(x, 0.75);
+			}
+		),
+		{ Color::Blue, 4.0f, 11.0f }
+	);
+	chart.addChart(
+		generate_chart(
+			0.0f, 1920.0f, 1.0f, 
+			[](float x)->float {
+				return pow(x, 0.80);
+			}
+		),
+		{ Color::Red, 1.0f, 12.0f }
+	);
+
+	while(window.isOpen()) {
+		if(window.pollEvent(event)) {
+			if(event.type == Event::KeyPressed) {
+				window.close();
+			}
+		}
+		chart.update();
+
+		window.clear(Color::White);
+		window.draw(chart);
+		window.display();
+	}
+
+	return 0;
+}
