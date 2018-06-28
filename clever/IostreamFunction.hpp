62ac6e904f78330c06f1ecf1379de4d43f8ca32f
clever411 <clever411@yandex.ru>
2018-06-28 08:40:27 +0300

    correct type

--- clever/IostreamFunction.hpp
+++ clever/IostreamFunction.hpp
@@ -1,1 +1,122 @@
-/usr/include/clever/IostreamFunction.hpp
+#ifndef CLEVER_IOSTREAM_FUNCTION_HPP
+#define CLEVER_IOSTREAM_FUNCTION_HPP
+
+#include <array>
+#include <vector>
+#include <utility>
+
+
+namespace clever
+{
+	template<class Ostream, typename T>
+	inline Ostream &print(Ostream &os, T const &t)
+	{
+		return os << t;
+	}
+
+	template<class Ostream, typename T>
+	inline Ostream &print(Ostream &os, T* t)
+	{
+		return os << '&' << *t;
+	}
+
+
+	template<class Ostream, typename T>
+	Ostream &print(
+		Ostream &os, std::vector<T> const &vec,
+		std::string const &term = ", "
+	)
+	{
+		os << "( ";
+		if(!vec.empty()) {
+			print(os, vec.front());
+			for(auto b = vec.begin()+1, e = vec.end(); b != e; ++b) {
+				print(os << term, *b);
+			}
+		}
+		os << " )";
+		return os;
+	}
+
+
+	template<class Ostream, typename T, typename U>
+	inline Ostream &print(Ostream &os, std::pair<T, U> const &pair)
+	{
+		return print(
+			print(os << "< ", pair.first) <<
+			", ", pair.second
+		) << " >";
+	}
+
+	template<class Ostream, typename T, typename U>
+	inline Ostream &print(
+		Ostream &os, std::pair<T, U> const &pair,
+		std::string const &term
+	)
+	{
+		return print(
+			print(os << "< ", pair.first) <<
+			term, pair.second
+		) << " >";
+	}
+
+
+	template<class Ostream, typename T, size_t N>
+	Ostream &print(
+		Ostream &os, std::array<T, N> const &arr,
+		std::string const &term = ", "
+	)
+	{
+		static_assert(N > 0);
+
+		os << "< " << arr[0];
+		for(size_t i = 1; i < N; ++i) {
+			print(os << term, arr[i]);
+		}
+		os << " >";
+		return os;
+	}
+
+
+	template<class Ostream, class Matrix>
+	Ostream &print_matrix(
+		Ostream &os, Matrix const &m, int w, int h,
+		std::string const &term = "\t"
+	)
+	{
+		for(int i = 0; i < h; ++i) {
+			for(int j = 0; j < w; ++j) {
+				print(os, m[i][j]) << '\t';
+			}
+			os << '\n';
+		}
+		return os;
+	}
+
+}
+
+
+
+template<class Ostream, typename T>
+inline Ostream &operator<<(Ostream &os, std::vector<T> const &vec)
+{
+	return clever::print(os, vec);
+}
+
+template<class Ostream, typename T, typename U>
+inline Ostream &operator<<(Ostream &os, std::pair<T, U> const &pair)
+{
+	return clever::print(os, pair);
+}
+
+template<class Ostream, typename T, size_t N>
+inline Ostream &operator<<(Ostream &os, std::array<T, N> const &arr)
+{
+	return clever::print(os, arr);
+}
+
+
+
+
+
+#endif
--- clever/SFML/HelpFunctions.hpp
+++ clever/SFML/HelpFunctions.hpp
@@ -1,1 +1,146 @@
-/usr/include/clever/SFML/HelpFunctions.hpp
+#ifndef SFML_HELP_FUNCTIONS_HPP
+#define SFML_HELP_FUNCTIONS_HPP
+
+#include <cmath>
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+
+
+namespace clever
+{
+
+
+template<typename T, typename U>
+inline sf::Vector2<T> conversion(sf::Vector2<U> const &vec)
+{
+	return sf::Vector2<T>(T(vec.x), T(vec.y));
+}
+
+inline double angle(double x1, double y1, double x2, double y2)
+{
+	return std::acos(
+		(x1*x2 + y1*y2) /
+		(std::hypot(x1, y1) * std::hypot(x2, y2))
+	);
+}
+
+template<typename T, typename U>
+inline double angle(sf::Vector2<T> const &first, sf::Vector2<U> const &second)
+{
+	return angle(first.x, first.y, second.x, second.y);
+}
+
+constexpr static double const PI = 3.141592653589;
+inline double togradus(double angle)
+{
+	return 180.0f*angle/PI;
+}
+
+inline double toradian(double angle)
+{
+	return angle*PI/180.0f;
+}
+
+}
+
+template<typename T>
+inline std::ostream &operator<<(std::ostream &os, sf::Vector2<T> const &vec)
+{
+	return os << "(" << vec.x << ", " << vec.y << ")";
+}
+template<typename T>
+inline std::istream &operator>>(std::ostream &is, sf::Vector2<T> &vec)
+{
+	return is >> vec.x >> vec.y;
+}
+
+
+template<typename T>
+inline std::ostream &operator<<(std::ostream &os, sf::Rect<T> const &rect)
+{
+	return os << "(" << rect.left << ", " << rect.top <<
+		")&(" << rect.width << ", " << rect.height << ")";
+}
+template<typename T>
+inline std::istream &operator<<(std::istream &is, sf::Rect<T> &rect)
+{
+	return is >> rect.left >> rect.top >> rect.width >> rect.height;
+}
+
+
+
+template<typename T, typename U>
+inline sf::Vector2<decltype(T()+U())> operator+(
+	sf::Vector2<T> const &lhs,
+	sf::Vector2<U> const &rhs
+)
+{
+	return {lhs.x+rhs.x, lhs.y+rhs.y};
+}
+
+template<typename T, typename U>
+inline sf::Vector2<decltype(T()-U())> operator-(
+	sf::Vector2<T> const &lhs,
+	sf::Vector2<U> const &rhs
+)
+{
+	return {lhs.x-rhs.x, lhs.y-rhs.y};
+}
+
+template<typename T, typename U>
+inline sf::Vector2<decltype(T()*U())> operator*(
+	sf::Vector2<T> const &lhs,
+	sf::Vector2<U> const &rhs
+)
+{
+	return {lhs.x*rhs.x, lhs.y*rhs.y};
+}
+
+template<typename T, typename U>
+inline sf::Vector2<decltype(T()/U())> operator/(
+	sf::Vector2<T> const &lhs,
+	sf::Vector2<U> const &rhs
+)
+{
+	return {lhs.x/rhs.x, lhs.y/rhs.y};
+}
+
+template<typename T, typename U>
+inline sf::Vector2<decltype(T()/U())> operator*(
+	sf::Vector2<T> const &lhs,
+	U const &number
+)
+{
+	return {lhs.x*number, lhs.y*number};
+}
+
+template<typename T, typename U>
+inline sf::Vector2<decltype(T()/U())> operator*(
+	U const &number,
+	sf::Vector2<T> const &lhs
+)
+{
+	return {lhs.x*number, lhs.y*number};
+}
+
+template<typename T, typename U>
+inline sf::Vector2<decltype(T()/U())> operator/(
+	sf::Vector2<T> const &lhs,
+	U const &number
+)
+{
+	return {lhs.x/number, lhs.y/number};
+}
+
+template<typename T, typename U>
+inline sf::Vector2<decltype(T()/U())> operator/(
+	U const &number,
+	sf::Vector2<T> const &lhs
+)
+{
+	return {lhs.x/number, lhs.y/number};
+}
+
+#endif // SFML_HELP_FUNCTIONS_HPP
+
